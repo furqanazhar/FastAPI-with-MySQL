15a1ba9661fb3e9c2b17d5212bfd3f0227bf8293
Furqan Azhar <furqanazhar92@gmail.com>
2022-08-09 15:35:27 +0500

    Loop over customers extracted from API

--- router/customer.py
+++ router/customer.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, status, Request
 from fastapi.responses import JSONResponse
 from common.utils import convert_response_to_json
 from database.db_helper import Database
+from model.customer import Customer
 import requests
 import traceback
 
@@ -33,19 +34,26 @@ async def populate_customer_data(limit: int):
             recordsList = response["pageItems"]
 
             print('Total Customers : ', totalRecords)
+            customerList:Customer = []
+            clientID = fullName = mobileNo = officeName = statusValue = None
 
             for i in range(len(recordsList)):
                 try:
-                    print('.......')
-                    print(recordsList[i]['id'])
-                    print(recordsList[i]['fullname'])
-                    print(recordsList[i]['mobileNo'])
-                    print(recordsList[i]['officeName'])
-                    print(recordsList[i]['status']['value'])
-                    print('.......')
-                except Exception as ex:
-                    error = traceback.print_tb(ex.__traceback__)
-                    print(error)
+                    clientID = recordsList[i].get('id','N/A')
+                    fullName = recordsList[i].get('fullname','N/A')
+                    mobileNo = recordsList[i].get('mobileNo','N/A')
+                    officeName = recordsList[i].get('officeName','N/A')
+                    statusValue = recordsList[i].get('status','N/A').get('value','N/A')
+                except Exception:
+                    print('error', traceback.format_exc())
+                finally:
+                    if clientID != 'N/A' and mobileNo != 'N/A':
+                        customer = Customer(clientID, fullName, mobileNo, officeName, statusValue)
+                        customerList.append(customer)
+                        print('Record inserted')
+                    else:
+                        print('Record not inserted because mobileNo/clientID is not available')
+
             payload = {
                 'message': 'Successfully created resource',
                 'data': convert_response_to_json(data)
