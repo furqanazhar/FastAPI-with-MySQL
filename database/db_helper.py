9f72d5b130056ef388ab369a78be6e5a35dea4fa
Furqan Azhar <furqanazhar92@gmail.com>
2022-08-10 05:55:52 +0500

    Add method to fetch all records from mysql db

--- database/db_helper.py
+++ database/db_helper.py
@@ -16,18 +16,18 @@ class Database:
         if self.connection.is_connected():
             dbInfo = self.connection.get_server_info()
             print("Connected to MySQL Server version ", dbInfo)
-            with open('database\create_table.sql', 'r') as f:
+            with open('database\create_table.sql', 'r') as file:
                 with self.connection.cursor() as cursor:
-                    cursor.execute(f.read(), multi=True)
+                    cursor.execute(file.read(), multi=True)
                 self.connection.commit()
         else:
             print("Connection to MySQL Server failed")
 
     async def insert_row(self, data):
         try:
-            with open('database\insert_row.sql', 'r') as f:
+            with open('database\insert_row.sql', 'r') as file:
                 with self.connection.cursor() as cursor:
-                    cursor.execute(f.read(), data)
+                    cursor.execute(file.read(), data)
                     self.connection.commit()
         except Exception as ex:
             print('error', ex)
@@ -36,4 +36,11 @@ class Database:
         pass
 
     async def get_all_rows(self):
-        pass
+        try:
+            with open('database\get_all.sql', 'r') as file:
+                with self.connection.cursor() as cursor:
+                    cursor.execute(file.read())
+                    customers = cursor.fetchall()
+                    return customers
+        except Exception as ex:
+            print('error', ex)
